cb9f15937ae7aaa0fb154c35c32fbc9f3bc2ec1c
Uonsaist <uonsaist.1@gmail.com>
2022-12-03 03:56:26 +0800

    change admin panel

--- admin/trunk.php
+++ admin/trunk.php
@@ -1,30 +1,50 @@
 <?php
 session_start();
+require_once '../include/connect.php';
+$result = mysqli_query($connect, "SELECT * FROM `request`");
 ?>
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <title>админка</title>
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
   </head>
   <body>
-    <?php if(!empty($_SESSION['staff']['first_name'])):?>
-      <div class="d-flex flex-column flex-md-row align-items-center p-3
-      px-mb-4 mb-3 bg-white border-bottom shadow-sm">
-        <h5 class="my-0 mr-md-auto font-weight-normal">СофиАрт</h5>
-        <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-          <a class="p-2 text-dark text-decoration-none" href="">Главная</a>
-        </nav>
 
-        <a href="loggout.php" class="btn btn-primary ">Выход</a>
-        <a style="margin: 2px 0;margin-left: 5px;"><?= $_SESSION['staff']['first_name'] ?></a>
-        <?php else:
-          echo '<h3 class="p-2 text-dark text-decoration-none">Отказано</h3>';
-          echo '<a href="/" class="btn btn-primary" >На главную</a>';
+  <?php require "header1.php" ?>
+
+<div  class="container mt-5">
+  <table  width="1000" class="sold">
+   <thead>
+    <tr>
+      <th>№</th>
+      <th>Фамилия</th>
+      <th>Имя</th>
+      <th>Описание</th>
+    </tr>
+   </thead>
+  <tbody>
+
+    <?php
+      while ($chart = mysqli_fetch_assoc($result)) {
       ?>
+    <tr>
+      <td><?php echo $chart['id']; ?></td>
+      <td><?php echo $chart['last_name']; ?></td>
+      <td><?php echo $chart['first_name']; ?></td>
+      <td><?php echo $chart['description']; ?></td>
+    </tr>
+    <?php
+  }
+    ?>
 
-      <?php endif ?>
-    </div>
+  </tbody>
+</table>
+</div>
+
+
+
   </body>
 </html>
