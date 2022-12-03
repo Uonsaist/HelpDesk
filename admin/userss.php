8a03713535f046e326efc3640c44dac67541841d
Uonsaist <uonsaist.1@gmail.com>
2022-12-03 22:36:37 +0800

    end

--- admin/userss.php
+++ admin/userss.php
@@ -12,7 +12,21 @@ $result = mysqli_query($connect, "SELECT * FROM `users`");
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
   </head>
   <body>
-      <?php require "header1.php" ?>
+    <?php if(!empty($_SESSION['staff']['first_name'])):?>
+      <div class="d-flex flex-column flex-md-row align-items-center p-3
+      px-mb-4 mb-3 bg-white border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto font-weight-normal">СофиАрт</h5>
+        <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+          <a class="p-2 text-dark text-decoration-none" href="trunk.php">Заявки</a>
+          <a class="p-2 text-dark text-decoration-none" href="section.php">Статьи</a>
+          <a class="p-2 text-dark text-decoration-none" href="userss.php">Пользователи</a>
+        </nav>
+
+        <a href="loggout.php" class="btn btn-primary ">Выход</a>
+        <a style="margin: 2px 0;margin-left: 5px;"><?= $_SESSION['staff']['first_name'] ?></a>
+
+    </div>
+
 
       <div  class="container mt-5">
         <table  width="1000" class="sold">
@@ -50,5 +64,11 @@ $result = mysqli_query($connect, "SELECT * FROM `users`");
       </table>
       </div>
         <?php require "foother1.php" ?>
+      <?php else:
+        echo '<h3 class="p-2 text-dark text-decoration-none">Отказано</h3>';
+        echo '<a href="/" class="btn btn-primary" >На главную</a>';
+    ?>
+
+    <?php endif ?>
   </body>
 </html>
