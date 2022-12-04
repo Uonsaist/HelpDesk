cd9040e7ab648ca045211111775758b00fe1c0bd
Uonsaist <uonsaist.1@gmail.com>
2022-12-04 15:28:13 +0800

    add sql file

---
+++ helpdesk (2).sql
@@ -0,0 +1,194 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Дек 04 2022 г., 08:25
+-- Версия сервера: 10.4.25-MariaDB
+-- Версия PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `helpdesk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `header` varchar(255) DEFAULT NULL,
+  `description` mediumtext DEFAULT NULL,
+  `staff_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `articles`
+--
+
+INSERT INTO `articles` (`id`, `header`, `description`, `staff_id`) VALUES
+(2, 'Нужен ли обогрев кровли?', 'Всей кровле обогрев не нужен и дорого обойдётся. Он крайне необходим при образовании сосулек на кровле и угрозе повреждения ими. Но следует понимать, что обогревать в таком случае приходится не только карниз или ендову, а и водосточные желоба с трубами. В любом случае решение надо принимать индивидуально исходя и из соображений безопасности.', NULL),
+(3, 'Какой профиль выбрать?', 'Вид профиля определяется только вашими предпочтениями. Обратить внимание нужно только на то, что некоторые виды требуют редких дополнительных элементов. На кровлях большой площади для лучшего визуального восприятия стараются использовать профили с «большой» по внешнему виду черепичкой, а на высоких кровлях профили с повышенной высотой ступеньки.', NULL),
+(4, 'Почему видны стыки листов?', 'Листы материала стыкуются путём нахлёста, и поэтому стык будет виден всегда. При этом материал должен быть плотно прижат, и обычно линия стыка не бросается в глаза. Если в местах стыка видны щели, то это чаще всего говорит либо о некачественном прокате материала с нарушением геометрической точности, либо о неквалифицированном монтаже.', NULL),
+(5, 'Ставить или нет снегозадержание?', 'Снегозадержание необходимо для защиты от падения глыб льда и массового схода снега. Однако оно же создаёт серьёзную весовую нагрузку на кровлю в местах скопления снега, поэтому его установка должна быть предусмотрена на стадии проектирования. При любых сомнениях в необходимости установки, следует принимать решение в пользу безопасности.', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `request`
+--
+
+CREATE TABLE `request` (
+  `id` int(11) NOT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `description` mediumtext DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `request`
+--
+
+INSERT INTO `request` (`id`, `last_name`, `first_name`, `description`, `user_id`) VALUES
+(2, 'Ujdjhby', 'sdfasefweaf', 'awefawefawefawef', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `staff`
+--
+
+INSERT INTO `staff` (`id`, `first_name`, `login`, `password`) VALUES
+(1, 'admin', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `login` varchar(100) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `password` varchar(500) DEFAULT NULL,
+  `staff_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `last_name`, `login`, `first_name`, `password`, `staff_id`) VALUES
+(1, 'Макаров', '123', 'Даниил', '202cb962ac59075b964b07152d234b70', 1);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `staff_id` (`staff_id`);
+
+--
+-- Индексы таблицы `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `staff_id` (`staff_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `request`
+--
+ALTER TABLE `request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `articles`
+--
+ALTER TABLE `articles`
+  ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `request`
+--
+ALTER TABLE `request`
+  ADD CONSTRAINT `request_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
